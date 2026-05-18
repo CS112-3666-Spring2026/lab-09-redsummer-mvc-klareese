d0398022a6a3d64c4c56d386be6512bcdb576950
ktrinidad05 <trinidadklareeseann@gmail.com>
2026-05-18 15:24:05 +0000

    DONE

--- src/main/java/cs112/lab09/MainApplication.java
+++ src/main/java/cs112/lab09/MainApplication.java
@@ -1,3 +1,4 @@
+
 package cs112.lab09;
 
 import javafx.application.Application;
@@ -5,16 +6,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class MainApplication extends Application {
+
+    private static Stage primaryStage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+
+        primaryStage = stage;
+
+        FXMLLoader loader = new FXMLLoader(
+                MainApplication.class.getResource("/cs112/lab09/main-view.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Red Summer MVC");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void changeScene(String fxml) {
+
+        try {
+
+            FXMLLoader loader = new FXMLLoader(
+                    MainApplication.class.getResource(fxml)
+            );
+
+            Scene scene = new Scene(loader.load());
+
+            primaryStage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/cs112/lab09/controllers/MainController.java
+++ src/main/java/cs112/lab09/controllers/MainController.java
@@ -1,14 +1,21 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.MainApplication;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        System.out.println("Hello button clicked");
+
+        welcomeText.setText("Loading map...");
+
+        MainApplication.changeScene("/cs112/lab09/map-view.fxml");
     }
 }
